770588b4479438b6cfa8b114caa4263abcc1135c
jettangs <jettangs@gmail.com>
2016-12-04 09:27:50 +0800

    fix

--- Component.js
+++ Component.js
@@ -1,40 +1,48 @@
-import React from 'react'
-import { render } from 'react-dom'
-import reactCSS from 'reactcss'
-import { wrap } from 'react-bounds'
+import React from 'react';
+import ReactCSS from 'reactcss';
+import bounds from 'react-bounds';
 
-class Component extends React.Component{
-  static bounds() {
+
+class Component extends ReactCSS.Component {
+
+  classes() {
     return {
-      'mobile': {
-        minWidth: 0,
-        maxWidth: 500,
+      'default': {
+        page: {
+          width: '960px',
+          height: '400px',
+          margin: '0 auto',
+          background: 'red'
+        },
+      },
+      'no-sidebar': {
+        sidebar: {
+          display: 'none',
+        },
+      },
+      'bigger-content': {
+        page: {
+          width: '1150px',
+        },
       },
     };
   }
 
-  render(){
-    var styles = reactCSS({
-      'default': {
-        body: {
-          background: 'red'
-        },
-      },
-      'mobile': {
-        body: {
-          background: 'blue'
-        },
-      },
-    })
+  static bounds() {
+    return {
+      'no-sidebar': { maxWidth: 520 },
+      'bigger-content': { minWidth: 1200 },
+    };
+  }
 
+  render() {
     return (
-      <div style={ styles.body }>
-        Hello
+      <div is="page">
+        <div is="content">gweg</div>
+        <div is="sidebar">wegweg</div>
       </div>
     );
   }
 }
 
-export default wrap(Component)
-
-
+export default bounds.wrap(Component);
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import reactCSS from 'reactcss'
 import { wrap } from 'react-bounds'
+import Schedule from './Schedule'
 import Component from './Component'
 
 
